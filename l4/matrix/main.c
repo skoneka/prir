a4c155fb64a94b9bda5bf4e860006eab7a28a548
Artur Skonecki <skoneka@gmail.com>
2013-03-17 18:14:18 +0100

    komentarz

--- l4/matrix/main.c
+++ l4/matrix/main.c
@@ -1,9 +1,35 @@
+/*
+Ćwiczenie PRIR - wątki
+Artur Skonecki, Łukasz Załęski
+Stworzenie indeksów określających podział danych obliczeniowych
+https://github.com/skoneka/prir/blob/master/l4/matrix/main.c
+--includes a cool thread pooling functionality--
+*/
+
+/* Co ten program robi:
+ * Wczytanie macierzy z pliku oraz inicjalizacja macierzy wynikowej.
+ *
+ * Utworzenie tablicy na wątki o rozmiarze MAX_THREADS
+ *
+ * Pobranie wolnego wątku z puli za pomocą funkcji getFreeThread()
+ *
+ * Obliczenie cząstkowych wyników mnożenia przez poszczególne wątki.
+ *
+ * Obliczenie w poszczególnych wątkach sumy elementów fragmentów macierzy i zapisanie ich do zmiennej globalnej.
+ *
+ * Zebranie statusów zakończenia wątków.
+ *
+ * Wypisanie wyniku na ekran. (macierzy wynikowej oraz sumy elementów)
+ *
+ * Zwolnienie pamięci.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
-#define MAX_THREADS 3
-
+#define MAX_THREADS 10
 
 typedef struct matrix_s
 {
@@ -29,21 +55,24 @@ typedef struct thread_data_s
 int checkFreeThread(int id);
 void setThreadState(int id, char state);
 char getThreadState(int id);
-int getFreeThread(int N);
-void mulMatrix(matrix_data_t *m, int N);
+int getFreeThread();
+void mulMatrix(matrix_data_t *m);
 void makeMatrix(matrix_t *m);
 void freeMatrix(matrix_t *m);
-int fileToMatrix(matrix_data_t *m, char *fileName);
+int filesToMatrix(matrix_data_t *m, char *fileNameA, char *fileNameB);
 void printMatrix(matrix_t *m);
+void sumCMatrixElements(matrix_data_t *m);
 
+double theSum = 0;
 pthread_t threads[MAX_THREADS]; /* array initialized to NULLs */
 int activeThreads[MAX_THREADS]; /* array initialized to 1 */
 /*
- * activeThreads[id] > 0 - wolny Thread
- * activeThreads[id] = 1 - nie uzyty thread
+ * activeThreads[id] > 0 - zajety Thread
+ * activeThreads[id] = 1 - nie uzyty thread lub zabezpieczony thread po pthread_join
  * activeThreads[id] = 2 - uzyty ale wolny thread
  */
 pthread_mutex_t threadMutex = NULL;
+
 
 int checkFreeThread(int id)
 {
@@ -60,7 +89,7 @@ int checkFreeThread(int id)
 void setThreadState(int id, char state)
 {
     pthread_mutex_lock(&threadMutex);
-    printf("setThreadState id=%d , state=%d\n", id, state);
+    /* VERBOSE fprintf(stderr, "setThreadState id=%d , state=%d\n", id, state); */
     activeThreads[id] = state;
     pthread_mutex_unlock(&threadMutex);
 }
@@ -74,16 +103,18 @@ char getThreadState(int id)
     return state;
 }
 
-int getFreeThread(int N)
+//int getUnusedThread
+
+int getFreeThread()
 {
     int i = 0;
     while(1) {
-        if (i == N)
+        if (i == MAX_THREADS)
             i = 0;
 
-        fprintf(stderr, "checkFreeThread(%d)\n", i);
+        /* VERBOSE fprintf(stderr, "checkFreeThread(%d)\n", i); */
         if(checkFreeThread(i)) {
-        fprintf(stderr, "checkFreeThread(%d) = %d\n", i, i);
+        /* VERBOSE fprintf(stderr, "checkFreeThread(%d) = %d\n", i, i); */
             return i;
         }
         i++;
@@ -92,7 +123,7 @@ int getFreeThread(int N)
 
 void *mulThread(void *ptr)
 {
-    printf("Thread number %ld\n", pthread_self());
+    /* VERBOSE fprintf(stderr, "Thread number %ld\n", pthread_self()); */
     thread_data_t *threadData = (thread_data_t*)ptr;
     int thID = threadData->threadID;
     matrix_data_t *m = threadData->m;
@@ -108,9 +139,95 @@ void *mulThread(void *ptr)
 
     free(threadData);
     setThreadState(thID, 2); // Free thread
+    pthread_exit((void *)0);
 }
 
-void mulMatrix(matrix_data_t *m, int N)
+
+void *sumThread(void *ptr)
+{
+    /* VERBOSE fprintf(stderr, "Thread number %ld\n", pthread_self()); */
+
+    thread_data_t *threadData = (thread_data_t*)ptr;
+    int thID = threadData->threadID;
+    matrix_data_t *m = threadData->m;
+    double s  = 0;
+    int i = threadData->i;
+    int k = 0;
+    for(k; k < m->C.colums; k++)
+    {
+        s+= m->C.M[i][k];
+    }
+    
+    pthread_mutex_lock(&threadMutex);
+    theSum += s;
+    pthread_mutex_unlock(&threadMutex);
+
+    free(threadData);
+    setThreadState(thID, 2); // Free thread
+    
+    pthread_exit((void *)0);
+}
+
+void sumCMatrixElements(matrix_data_t *m)
+{
+    int i, j;
+
+    for ( i = 0; i < m->C.rows; i++ ) {
+        int ret;
+        int thID = getFreeThread();
+
+        thread_data_t *threadData = malloc(sizeof(thread_data_t));
+        threadData->i = i;
+        threadData->threadID = thID;
+        threadData->m = m;
+        if(activeThreads[thID] == 2) //juz raz uzylismy...
+        {
+            /* VERBOSE fprintf(stderr, "Reusing thread... securing with pthread_join just in case\n"); */
+            int *val;
+            ret = pthread_join(threads[thID], (void **)&val);
+            /* VERBOSE fprintf(stderr, "Thread %d exit status %d\n", thID, val);  */
+            if( ret != 0) {
+                /* VERBOSE fprintf(stderr, "%s:%d pthread_join fail %d\n", __FILE__, __LINE__, ret); */
+                exit(EXIT_FAILURE);
+            }
+            else {
+                /* VERBOSE fprintf(stderr, "%s:%d pthread_join OK\n", __FILE__, __LINE__); */
+                activeThreads[thID] = 1;
+            }
+        }
+
+        setThreadState(thID, 0);// Busy
+
+        //raise(SIGINT);
+
+        /* VERBOSE fprintf(stderr, "Thread %d\n", thID);  */
+        ret = pthread_create( &threads[thID], NULL, sumThread, (void*) threadData); 
+        if( ret != 0) {
+            /* VERBOSE fprintf(stderr, "%s:%d pthread_create fail %d\n", __FILE__, __LINE__, ret); */
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (i = 0; i < MAX_THREADS;i++)
+    {
+        if(getThreadState(i) == 2 || getThreadState(i) == 0)
+        {
+            int *val;
+            int ret = pthread_join(threads[i], (void **)&val);
+            /* VERBOSE fprintf(stderr, "Thread %d exit status %d\n", i, val);  */
+            if( ret != 0) {
+                /* VERBOSE fprintf(stderr, "%s:%d pthread_join fail %d\n", __FILE__, __LINE__, ret); */
+                exit(EXIT_FAILURE);
+            }
+            else {
+                /* VERBOSE fprintf(stderr, "%s:%d pthread_join OK\n", __FILE__, __LINE__); */
+                activeThreads[i] = 1;
+            }
+        }
+    }
+}
+
+
+void mulMatrix(matrix_data_t *m)
 {
     int i, j, k;
 
@@ -119,42 +236,57 @@ void mulMatrix(matrix_data_t *m, int N)
         for(j=0; j< m->B.colums; j++)
         {
             int ret;
-            int thID = getFreeThread(N);
+            int thID = getFreeThread();
 
             thread_data_t *threadData = malloc(sizeof(thread_data_t));
             threadData->i = i;
             threadData->j = j;
             threadData->threadID = thID;
             threadData->m = m;
-            printf("Czekamy\n");
             if(activeThreads[thID] == 2) //juz raz uzylismy...
             {
-                printf("Czekamy\n");
-                ret = pthread_join(threads[thID], NULL);
+                /* VERBOSE fprintf(stderr, "Reusing thread... securing with pthread_join just in case\n"); */
+                int *val;
+                ret = pthread_join(threads[thID], (void **)&val);
+                /* VERBOSE fprintf(stderr, "Thread %d exit status %d\n", thID, val);  */
                 if( ret != 0) {
-                    fprintf(stderr, "%s:%d pthread_join fail %d\n", __FILE__, __LINE__, ret);
+                    /* VERBOSE fprintf(stderr, "%s:%d pthread_join fail %d\n", __FILE__, __LINE__, ret); */
                     exit(EXIT_FAILURE);
                 }
-                else
-                    fprintf(stderr, "%s:%d pthread_join OK\n", __FILE__, __LINE__);
+                else {
+                    /* VERBOSE fprintf(stderr, "%s:%d pthread_join OK\n", __FILE__, __LINE__); */
+                    activeThreads[thID] = 1;
+                }
             }
 
             setThreadState(thID, 0);// Busy
 
             //raise(SIGINT);
+
+            /* VERBOSE fprintf(stderr, "Thread %d\n", thID);  */
             ret = pthread_create( &threads[thID], NULL, mulThread, (void*) threadData); 
             if( ret != 0) {
-                fprintf(stderr, "%s:%d pthread_create fail %d\n", __FILE__, __LINE__, ret);
+                /* VERBOSE fprintf(stderr, "%s:%d pthread_create fail %d\n", __FILE__, __LINE__, ret); */
                 exit(EXIT_FAILURE);
             }
         }
     }
 
-    for (i = 0; i < N;i++)
+    for (i = 0; i < MAX_THREADS;i++)
     {
         if(getThreadState(i) == 2 || getThreadState(i) == 0)
         {
-            pthread_join(threads[i], NULL);
+            int *val;
+            int ret = pthread_join(threads[i], (void **)&val);
+            /* VERBOSE fprintf(stderr, "Thread %d exit status %d\n", i, val);  */
+                if( ret != 0) {
+                    /* VERBOSE fprintf(stderr, "%s:%d pthread_join fail %d\n", __FILE__, __LINE__, ret); */
+                    exit(EXIT_FAILURE);
+                }
+                else {
+                    /* VERBOSE fprintf(stderr, "%s:%d pthread_join OK\n", __FILE__, __LINE__); */
+                    activeThreads[i] = 1;
+                }
         }
     }
 }
@@ -184,16 +316,17 @@ void freeMatrix(matrix_t *m)
     free(m->M);
 }
 
-int fileToMatrix(matrix_data_t *m, char *fileName)
+int filesToMatrix(matrix_data_t *m, char *fileNameA, char *fileNameB)
 {
-    FILE *f = fopen(fileName, "r");
+    FILE *fA = fopen(fileNameA, "r");
+    FILE *fB = fopen(fileNameB, "r");
     int i,j;
 
-    if (!f)
+    if (!fA || !fB)
         return 1;
 
-    fscanf(f, "%d %d", &m->A.rows, &m->A.colums);
-    fscanf(f, "%d %d", &m->B.rows, &m->B.colums);
+    fscanf(fA, "%d %d", &m->A.rows, &m->A.colums);
+    fscanf(fB, "%d %d", &m->B.rows, &m->B.colums);
 
     makeMatrix(&m->A);
     makeMatrix(&m->B);
@@ -205,7 +338,7 @@ int fileToMatrix(matrix_data_t *m, char *fileName)
     {
         for (j = 0; j < m->A.colums;j++)
         {
-            fscanf(f, "%lf", &(m->A.M[i][j]));
+            fscanf(fA, "%lf", &(m->A.M[i][j]));
         }
     }
 
@@ -213,10 +346,11 @@ int fileToMatrix(matrix_data_t *m, char *fileName)
     {
         for (j = 0; j < m->B.colums;j++)
         {
-            fscanf(f, "%lf", &(m->B.M[i][j]));
+            fscanf(fB, "%lf", &(m->B.M[i][j]));
         }
     }
-    fclose(f);
+    fclose(fA);
+    fclose(fB);
 
     return 0;
 }
@@ -245,13 +379,24 @@ int main(void)
 {
     init();
     matrix_data_t m = {0};
-    fileToMatrix(&m, "matrix.txt");
-    mulMatrix(&m, 4);
+    filesToMatrix(&m, "A.txt", "B.txt");
+
+    /* A*B=C using thread pool */
+    mulMatrix(&m);
+
+    /* return codes of threads */
+
     printMatrix(&m.A);
     printf("---\n");
     printMatrix(&m.B);
     printf("---\n");
     printMatrix(&m.C);
+    printf("---\n");
+
+    /* mutex use show off */
+    /* VERBOSE fprintf(stderr, "Sum C with threads\n"); */
+    sumCMatrixElements(&m);
+    printf("Sum = %f\n", theSum);
 
     freeMatrix(&m.A);
     freeMatrix(&m.B);
