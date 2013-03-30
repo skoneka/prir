9b12c39b0ca3cffa24eefd305542bba72c1e23f4
Artur Skonecki <skoneka@gmail.com>
2013-03-30 23:01:11 +0100

    l6py setup optparse

--- l6/test_apachelog.py
+++ l6/test_apachelog.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Artur Skonecki, Łukasz Załęski
+# python <3
+# 2013 Artur Skonecki, Łukasz Załęski
 import apachelog, sys
 from mpi4py import MPI
 from operator import itemgetter
@@ -10,15 +11,27 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-
 if rank == 0:
+  from optparse import OptionParser
+  
+  parser = OptionParser(usage = 'Usage: apachelogstats.py --(addr|stat|time) /var/log/apache2/access.log.1')
+  
+  parser.add_option("-t", "--time",
+  action="store_const", dest="mode", const = '%t',
+  help="group by time")
+  parser.add_option("-a", "--addr",
+  action="store_const", dest="mode", const = '%h',
+  help="group by address")
+  
+  (options, args) = parser.parse_args()
+  
   # Format copied and pasted from Apache conf - use raw string + single quotes
   format = r'%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
 
   p = apachelog.parser(format)
   data = []
 
-  for i,line in enumerate(open('/var/log/apache2/access.log.1')):
+  for i,line in enumerate(open(args[0])):
     try:
       data.append( p.parse( line ) )
     except:
@@ -26,6 +39,9 @@ if rank == 0:
     if i > 6: break
 else:
   data = None
+  options = None
+
+options = comm.bcast(options, root=0)
 
 
 # grouper '%h' | '%t'
@@ -38,25 +54,18 @@ def MR_map(data, grouper):
     extended_data = data[size * basic_range:size * basic_range + extended_range]
     divided_data[0] += extended_data
   divided_data = comm.scatter(divided_data, root=0)
-  print('rank '+ str(rank) +' len divided_data '+str(len(divided_data)))
-  #print('rank '+ str(rank) +' data '+str(data[0]))
+  #print('rank '+ str(rank) +' len divided_data '+str(len(divided_data)))
   divided_data = sorted(divided_data, key=itemgetter(grouper))
   groups = defaultdict(list)
   for key, group in groupby(divided_data, itemgetter(grouper)):
     for thing in group:
       groups[key].append( thing )
-      print ('rank %d at %s append ' % (rank, str(key)))
-      #print "A %s is a %s." % (thing['%r'], key)
-  #print( groups )
   data = comm.gather(groups, root=0)
-  if rank == 0:
-    for item in data:
-      print len(item)
-  else:
+  if rank != 0:
     assert data is None
   return data
-    
-ranked_data = MR_map(data, '%t')
+
+ranked_data = MR_map(data, options.mode)
 
 # merge all entries from ranked data dicts
 def MR_reduce(ranked_data):
@@ -69,7 +78,6 @@ def MR_reduce(ranked_data):
 if rank == 0:
   dd = MR_reduce(ranked_data)
   
-  #print dd
   for key, value in dd.iteritems():
     print( '%s %d' % ( str(key), len(value) ) )
 
